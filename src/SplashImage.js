c14e476283532d29ea85483aa2b02c0ec7c880c1
GGmj <dhkswo1@gmail.com>
2023-05-31 19:49:07 +0900

    feat: useNavigator 추가

--- src/SplashImage.js
+++ src/SplashImage.js
@@ -2,45 +2,49 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import SplashIm from "./image/main.png";
 import ConnectIm from "./image/secondMain.png";
-import VoiceChoice from "./VoiceChoice";
-import Camera from "./Camera";
-import "./SplashImage.moduel.css";
+import "./SplashImage.module.css";
+import { useNavigate } from "react-router-dom";
 
 function SplashImage() {
   const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+  console.log(navigate);
   const formData = new FormData();
   formData.append("token_id", "temp");
+
   useEffect(() => {
     axios
-      .post("http://192.168.0.39:80/settings", formData, {
+      .post("http://192.168.0.77:8443/settings", formData, {
         headers: {
-          "Content-Type": "multipart/form-data",
+          "Content-Type": "application/json",
         },
       })
       .then((response) => {
         setData(response.data);
-        console.log(response.data);
-        setIsLoading(false);
+
+        // sessionStorage.setItem(쟤가 만든 테이블 컬럼이름 key , 쟤가 만든 테이블 컬럼의 값 );
+
+        if (response.data) {
+          console.log(response.data);
+          navigate("/voiceChoice/speedChoice/camera", { replace: true });
+        } else {
+          navigate("/voiceChoice", { replace: true });
+        }
       })
       .catch((error) => {
         console.log(error);
-        setData(null);
-        setIsLoading(false);
+        navigate("/voiceChoice", { replace: true });
       });
-  }, []);
+  }, [navigate]);
 
   return (
-    <>
-      <div className="splashScreen">
-        <img
-          src={isLoading ? SplashIm : ConnectIm}
-          alt={isLoading ? "시작화면" : "접속완료 화면"}
-          className={isLoading ? "fade-out" : "fade-in"}
-        />
-      </div>
-      <div>{data ? <Camera /> : <VoiceChoice />}</div>
-    </>
+    <div className="splashScreen">
+      {data === null ? (
+        <img src={SplashIm} alt="시작화면" className="fade-in" />
+      ) : (
+        <img src={ConnectIm} alt="접속완료 화면" className="fade-in-fade-out" />
+      )}
+    </div>
   );
 }
 
