63c12c7ca1ec13adb36a892973fb386db63dff22
1three <1_three@naver.com>
2023-05-31 19:38:06 +0900

    feat: UI/UX 최신화

--- src/SpeedChoice.js
+++ src/SpeedChoice.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
+import { Box, Button } from "@mui/material";
 import Header from "./components/Header";
 import IconButton from "@mui/material/IconButton";
 import KeyboardControlKeyOutlinedIcon from "@mui/icons-material/KeyboardControlKeyOutlined";
 import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
-const App = () => {
+
+const SpeedChoice = () => {
   return (
     <>
-      <Header />
+      <Header title="음성 속도 조절" skipLink="/splashImage/voiceChoice/speedChoice/camera"/>
       <Box
         sx={{
           display: "flex",
@@ -67,7 +67,7 @@ const App = () => {
           <Button
             variant="Outlined"
             component={Link}
-            to="/voiceChoice/speedChoice/camera"
+            to="/splashImage/voiceChoice/speedChoice/camera"
             sx={{
               backgroundColor: "white",
               borderRadius: "25px",
@@ -84,4 +84,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default SpeedChoice;
