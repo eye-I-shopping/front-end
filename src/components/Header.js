73ad7043a28a2ba947d70185b034a167139977e3
1three <1_three@naver.com>
2023-06-02 00:04:58 +0900

    feat: 건너뛰기 props 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,13 @@ import styles from "./Header.module.css";
 import LogoIm from "../image/headerLogo.png";
 import { Link } from "react-router-dom";
 
-function Header({ title, skipLink }) {
+function Header({ title, skipLink, skipOnClick }) {
   return (
     <Box className={styles.header}>
       <img className={styles.logo} src={LogoIm} alt="Logo" />
       <Typography variant="h5">{title}</Typography>
       <Box className={styles.skipButtonContainer}>
-        <Button color="inherit" component={Link} to={skipLink}>건너뛰기</Button>
+        <Button color="inherit" component={Link} to={skipLink} onClick={skipOnClick}>건너뛰기</Button>
       </Box>
     </Box>
   );
