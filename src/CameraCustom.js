a934b7f5d8f323679b57da8b57054dd6eeb5ffa7
GGmj <dhkswo1@gmail.com>
2023-06-04 15:45:52 +0900

    feat: 맞춤 정보 설정 페이지 UI/UX 최신화

--- src/CameraCustom.js
+++ src/CameraCustom.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
+import {
+  Box,
+  Button,
+  Switch,
+  FormControlLabel,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
 import Header from "./components/Header";
-import Switch from "@mui/material/Switch";
-import FormControlLabel from "@mui/material/FormControlLabel";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#977CC9",
+    },
+  },
+});
 
 const Custom = () => {
   const [infoChoice, setInfoChoice] = useState({
@@ -14,8 +26,8 @@ const Custom = () => {
     cooking: false,
   });
 
-  const handleChange = (event) => {
-    setInfoChoice({ ...infoChoice, [event.target.name]: event.target.checked });
+  const handleToggle = (name) => () => {
+    setInfoChoice((prev) => ({ ...prev, [name]: !prev[name] }));
   };
 
   const handleSave = () => {
@@ -23,8 +35,45 @@ const Custom = () => {
     // TODO: Save to session storage or send to server
   };
 
+  const BoxOption = ({ name, icon, label }) => (
+    <Box
+      onClick={handleToggle(name)}
+      backgroundColor="lightgray"
+      sx={{
+        border: "2px solid white",
+        borderRadius: "25px",
+        padding: "10px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        cursor: "pointer",
+      }}
+    >
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        {icon}
+        <Box component="span" sx={{ marginLeft: 5 }}>
+          {label}
+        </Box>
+      </Box>
+      <ThemeProvider theme={theme}>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={infoChoice[name]}
+              onChange={() => {}}
+              name={name}
+              color="primary"
+            />
+          }
+          label=""
+          sx={{ marginLeft: "auto" }}
+        />
+      </ThemeProvider>
+    </Box>
+  );
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header
         title="맞춤 정보 설정"
         skipLink="/splashImage/custom/voiceChoice/speedChoice/camera"
@@ -51,107 +100,21 @@ const Custom = () => {
             marginBottom: "0",
             height: "85vh",
             backgroundColor: "#977CC9",
+            fontSize: "calc(1.5vw + 1.5vh)",
           }}
         >
-          <Box
-            sx={{
-              border: "2px solid white",
-              borderRadius: "25px",
-              padding: "10px",
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <FormControlLabel
-              control={
-                <Switch
-                  checked={infoChoice.taste}
-                  onChange={handleChange}
-                  name="taste"
-                  color="primary"
-                />
-              }
-              label="맛 정보 확인"
-              labelPlacement="start"
-            />
-          </Box>
-          <Box
-            sx={{
-              border: "2px solid white",
-              borderRadius: "25px",
-              padding: "10px",
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <FormControlLabel
-              control={
-                <Switch
-                  checked={infoChoice.allergy}
-                  onChange={handleChange}
-                  name="allergy"
-                  color="primary"
-                />
-              }
-              label="알레르기 정보 확인"
-              labelPlacement="start"
-            />
-          </Box>
-          <Box
-            sx={{
-              border: "2px solid white",
-              borderRadius: "25px",
-              padding: "10px",
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <FormControlLabel
-              control={
-                <Switch
-                  checked={infoChoice.package}
-                  onChange={handleChange}
-                  name="package"
-                  color="primary"
-                />
-              }
-              label="포장 형태 확인"
-              labelPlacement="start"
-            />
-          </Box>
-          <Box
-            sx={{
-              border: "2px solid white",
-              borderRadius: "25px",
-              padding: "10px",
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <FormControlLabel
-              control={
-                <Switch
-                  checked={infoChoice.cooking}
-                  onChange={handleChange}
-                  name="cooking"
-                  color="primary"
-                />
-              }
-              label="조리방법 및 주의사항 확인"
-              labelPlacement="start"
-            />
-          </Box>
+          <BoxOption name="taste" label="맛 정보 확인" />
+          <BoxOption name="allergy" label="알레르기 정보 확인" />
+          <BoxOption name="package" label="포장 형태 확인" />
+          <BoxOption name="cooking" label="조리방법 및 주의사항 확인" />
           <Button
             onClick={handleSave}
-            variant="outlined"
             color="primary"
+            variant="contained"
             component={Link}
             to="/splashImage/custom/voiceChoice/speedChoice/camera"
             sx={{
+              border: "2px solid white",
               backgroundColor: "white",
               borderRadius: "25px",
               color: "black",
@@ -162,7 +125,7 @@ const Custom = () => {
           </Button>
         </Box>
       </Box>
-    </>
+    </ThemeProvider>
   );
 };
 
